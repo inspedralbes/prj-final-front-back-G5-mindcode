5815a33d3e52696b6de3a2048b1bda7b9e30c534
arnaaudev <arnau.malaret4@gmail.com>
2025-04-21 23:46:36 +0200

    changes

--- front/src/app/PfSettings/page.js
+++ front/src/app/PfSettings/page.js
@@ -103,6 +103,7 @@ const PfSettings = () => {
                 )}
             </div>
         </div>
+        
         {isDialogOpen && (
             <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
                 <Dialog
