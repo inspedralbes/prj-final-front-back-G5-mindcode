b0850fc10f144d505411689324b56ca944b9977d
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-05-15 09:58:05 +0200

    attempt to fix cert error AI

--- back/routes/aiRoutes.js
+++ back/routes/aiRoutes.js
@@ -17,6 +17,7 @@ import { verifyTokenMiddleware } from "../tokens.js";
 import dotenv from 'dotenv';
 import Message from "../schemes/mongoScheme.js"
 import Quiz from "../schemes/quizScheme.js"
+import { Agent } from 'https';
 
 dotenv.config();
 
@@ -38,202 +39,202 @@ const __dirname = path.dirname(__filename);
  * @returns {Object} AI response or error message.
  */
 router.post('/create', verifyTokenMiddleware, async (req, res) => {
-    const { message, language_id, class_id } = req.body;
-    const verified_user_id = req.verified_user_id;
-    let connection;
-    const objToSaveMongoDB = {
-        userContent: message,
-        aiContent: "",
-        aiThought: "",
-        userId: verified_user_id,
-        language: "",
-        languageId: language_id,
-        classId: class_id
+  const { message, language_id, class_id } = req.body;
+  const verified_user_id = req.verified_user_id;
+  let connection;
+  const objToSaveMongoDB = {
+    userContent: message,
+    aiContent: "",
+    aiThought: "",
+    userId: verified_user_id,
+    language: "",
+    languageId: language_id,
+    classId: class_id
+  }
+
+  console.log("message", message);
+
+  // Validación del mensaje
+  if (!message || typeof message !== "string" || message.trim() === "") {
+    return res
+      .status(400)
+      .json({ error: "El mensaje es obligatorio y no puede estar vacío." });
+  }
+
+  console.log("type of language_id: ", typeof language_id);
+  // Validación del ID de idioma
+  if (!language_id || typeof language_id !== 'number') {
+    return res.status(400).json({ error: 'El ID de idioma es obligatorio y debe ser un número.' });
+  }
+
+
+  console.log("type of class_id: ", typeof class_id);
+  // Validación del ID de clase
+  if (!class_id || typeof class_id !== 'number') {
+    return res.status(400).json({ error: 'El ID de clase es obligatorio y debe ser un número.' });
+  }
+
+  console.log("Id del usuario: ", verified_user_id);
+
+  // Validación del ID de usuario verificado
+  if (!verified_user_id || typeof verified_user_id !== 'number') {
+    return res.status(400).json({ error: 'El ID de usuario verificado es obligatorio y debe ser un número.' });
+  }
+
+  try {
+    connection = await createConnection();
+    const [rows] = await connection.execute(
+      'SELECT id FROM USER WHERE id = ? AND class = ?',
+      [verified_user_id, class_id]
+    );
+
+    if (rows.length === 0) {
+      return res.status(401).json({ error: 'No perteneces a esta clase.' });
     }
 
-    console.log("message", message);
+  } catch (error) {
+    console.error('Error fetching class language:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    if (connection) connection.end();
+  }
 
-    // Validación del mensaje
-    if (!message || typeof message !== "string" || message.trim() === "") {
-        return res
-            .status(400)
-            .json({ error: "El mensaje es obligatorio y no puede estar vacío." });
+
+  let language;
+  let languageToSend;
+
+  try {
+    connection = await createConnection();
+    const [rows] = await connection.execute(
+      'SELECT language FROM CLASS WHERE idclass = ?',
+      [class_id]
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: 'Class not found' });
     }
 
-    console.log("type of language_id: ", typeof language_id);
-    // Validación del ID de idioma
-    if (!language_id || typeof language_id !== 'number') {
-        return res.status(400).json({ error: 'El ID de idioma es obligatorio y debe ser un número.' });
+    language = rows[0].language;
+
+    console.log("Language: ", language);
+
+  } catch (error) {
+    console.error('Error fetching class language:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    if (connection) connection.end();
+  }
+
+  const parsedLanguages = JSON.parse(language);
+  languageToSend = parsedLanguages.find((language) => language.id === language_id);
+
+  if (languageToSend === undefined) {
+    return res.status(400).json({ error: 'El lenguaje del mensaje no coincide con el lenguaje de la clase.' });
+  }
+
+  let restriction;
+
+  try {
+    connection = await createConnection();
+    const [rows] = await connection.execute(
+      'SELECT content FROM RESTRICTION WHERE idrestriction = ?',
+      [languageToSend.restrictionId]
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: 'Restriction not found' });
     }
 
+    restriction = rows[0].content;
+  } catch (error) {
+    console.error('Error fetching restriction:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    if (connection) connection.end();
+  }
 
-    console.log("type of class_id: ", typeof class_id);
-    // Validación del ID de clase
-    if (!class_id || typeof class_id !== 'number') {
-        return res.status(400).json({ error: 'El ID de clase es obligatorio y debe ser un número.' });
+  objToSaveMongoDB.language = languageToSend.name;
+
+  try {
+    console.log("Sending to AI");
+    const aiResponse = await sendToAI(message, languageToSend.name, restriction);
+
+    const returnMessage = aiResponse.content;
+
+    // Extract the content within <think> tags
+    const thinkTagContent = returnMessage.match(/<think>(.*?)<\/think>/s);
+
+    let restOfContent = "Sorry, something went wrong. Please try again.";
+
+    if (thinkTagContent && thinkTagContent[1]) {
+      const extractedContent = thinkTagContent[1];
+      console.log("Extracted Content: ", extractedContent);
+
+      restOfContent = returnMessage.replace(thinkTagContent[0], "").trim();
+      console.log("Rest of Content: ", restOfContent);
+    } else {
+      console.log("No <think> tag found in the response.");
     }
 
-    console.log("Id del usuario: ", verified_user_id);
+    objToSaveMongoDB.aiContent = restOfContent;
+    objToSaveMongoDB.aiThought = thinkTagContent ? thinkTagContent[1] : '';
 
-    // Validación del ID de usuario verificado
-    if (!verified_user_id || typeof verified_user_id !== 'number') {
-        return res.status(400).json({ error: 'El ID de usuario verificado es obligatorio y debe ser un número.' });
-    }
 
     try {
-        connection = await createConnection();
-        const [rows] = await connection.execute(
-            'SELECT id FROM USER WHERE id = ? AND class = ?',
-            [verified_user_id, class_id]
-        );
+      const connection = await createConnection();
+      const [result] = await connection.execute(
+        'UPDATE USER SET message_count = message_count + 1 WHERE id = ?',
+        [verified_user_id]
+      );
 
-        if (rows.length === 0) {
-            return res.status(401).json({ error: 'No perteneces a esta clase.' });
-        }
+      const [rows] = await connection.execute(
+        'SELECT message_count FROM USER WHERE id = ?',
+        [verified_user_id]
+      );
 
-    } catch (error) {
-        console.error('Error fetching class language:', error);
-        return res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        if (connection) connection.end();
-    }
+      const messageCount = rows[0].message_count;
+      console.log(`Contador de mensajes: ${messageCount}`);
 
-
-    let language;
-    let languageToSend;
-
-    try {
-        connection = await createConnection();
-        const [rows] = await connection.execute(
-            'SELECT language FROM CLASS WHERE idclass = ?',
-            [class_id]
-        );
-
-        if (rows.length === 0) {
-            return res.status(404).json({ error: 'Class not found' });
-        }
-
-        language = rows[0].language;
-        
-        console.log("Language: ", language);
-
-    } catch (error) {
-        console.error('Error fetching class language:', error);
-        return res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        if (connection) connection.end();
-    }
-
-    const parsedLanguages = JSON.parse(language);
-    languageToSend = parsedLanguages.find((language) => language.id === language_id);
-
-    if (languageToSend === undefined) {
-        return res.status(400).json({ error: 'El lenguaje del mensaje no coincide con el lenguaje de la clase.' });
-    }
-
-    let restriction;
-
-    try {
-        connection = await createConnection();
-        const [rows] = await connection.execute(
-            'SELECT content FROM RESTRICTION WHERE idrestriction = ?',
-            [languageToSend.restrictionId]
-        );
-
-        if (rows.length === 0) {
-            return res.status(404).json({ error: 'Restriction not found' });
-        }
-
-        restriction = rows[0].content;
-    } catch (error) {
-        console.error('Error fetching restriction:', error);
-        return res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        if (connection) connection.end();
-    }
-
-    objToSaveMongoDB.language = languageToSend.name;
-
-    try {
-        console.log("Sending to AI");
-        const aiResponse = await sendToAI(message, languageToSend.name, restriction);
-
-        const returnMessage = aiResponse.content;
-
-        // Extract the content within <think> tags
-        const thinkTagContent = returnMessage.match(/<think>(.*?)<\/think>/s);
-
-        let restOfContent = "Sorry, something went wrong. Please try again.";
-
-        if (thinkTagContent && thinkTagContent[1]) {
-            const extractedContent = thinkTagContent[1];
-            console.log("Extracted Content: ", extractedContent);
-
-            restOfContent = returnMessage.replace(thinkTagContent[0], "").trim();
-            console.log("Rest of Content: ", restOfContent);
-        } else {
-            console.log("No <think> tag found in the response.");
-        }
-
-      objToSaveMongoDB.aiContent = restOfContent;
-      objToSaveMongoDB.aiThought = thinkTagContent ? thinkTagContent[1] : '';
-      
-
-      try {
-        const connection = await createConnection();
-        const [result] = await connection.execute(
-          'UPDATE USER SET message_count = message_count + 1 WHERE id = ?',
-          [verified_user_id]
-        );
-
-        const [rows] = await connection.execute(
-          'SELECT message_count FROM USER WHERE id = ?',
-          [verified_user_id]
-        );
-
-        const messageCount = rows[0].message_count;
-        console.log(`Contador de mensajes: ${messageCount}`);
-
-        if (messageCount >= 5) {
-          console.log('5 mensajes detectados, generando quiz...');
-          const messages = await Message.find({ 
-            userId: verified_user_id,
-            classId: class_id 
-          })
+      if (messageCount >= 5) {
+        console.log('5 mensajes detectados, generando quiz...');
+        const messages = await Message.find({
+          userId: verified_user_id,
+          classId: class_id
+        })
           .sort({ _id: -1 })
           .limit(5)
           .lean();
 
-          const formattedMessages = messages.map(msg => msg.userContent).join('\n');
-          const quizResponse = await sendForQuiz(formattedMessages);
+        const formattedMessages = messages.map(msg => msg.userContent).join('\n');
+        const quizResponse = await sendForQuiz(formattedMessages);
 
-          if (quizResponse && quizResponse.quiz && Array.isArray(quizResponse.quiz)) {
-            const quiz = new Quiz({
-              userId: verified_user_id,
-              classId: class_id,
-              questions: quizResponse.quiz,
-              userAnswers: [],
-              createdAt: new Date()
-            });
-            await quiz.save();
-            console.log('Quiz generated and saved in MongoDB:', quiz._id);
+        if (quizResponse && quizResponse.quiz && Array.isArray(quizResponse.quiz)) {
+          const quiz = new Quiz({
+            userId: verified_user_id,
+            classId: class_id,
+            questions: quizResponse.quiz,
+            userAnswers: [],
+            createdAt: new Date()
+          });
+          await quiz.save();
+          console.log('Quiz generated and saved in MongoDB:', quiz._id);
 
-            await connection.execute(
-              'UPDATE USER SET message_count = 0 WHERE id = ?',
-              [verified_user_id]
-            );
-          }
+          await connection.execute(
+            'UPDATE USER SET message_count = 0 WHERE id = ?',
+            [verified_user_id]
+          );
         }
+      }
 
-        await connection.end();
-      } catch (error) {
-        console.error('Error al procesar mensajes:', error);
-      } 
+      await connection.end();
+    } catch (error) {
+      console.error('Error al procesar mensajes:', error);
+    }
 
-      res.status(200).json(restOfContent);
+    res.status(200).json(restOfContent);
   } catch (error) {
-      console.error("Error en el servidor:", error);
-      res.status(500).json({ error: "Internal server error" });
+    console.error("Error en el servidor:", error);
+    res.status(500).json({ error: "Internal server error" });
   } finally {
     saveMessage(objToSaveMongoDB);
   }
@@ -251,32 +252,33 @@ router.post('/create', verifyTokenMiddleware, async (req, res) => {
  * @throws Will throw an error if the AI service responds with an error or no response.
  */
 const sendToAI = async (message, language, restriction) => {
-    console.log("sending message");
-    const response = await fetch(`${AIHOST}`, {
-      method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ userPrompt: message, language, restriction })
-    });
+  console.log("sending message");
+  const response = await fetch(`${AIHOST}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ userPrompt: message, language, restriction }),
+    agent: new Agent({ rejectUnauthorized: false }) 
+  });
 
-    console.log("answer recieved");
+  console.log("answer recieved");
 
-    if (!response.ok) {
-        throw new Error("La IA respondió con un error: " + response.statusText);
-    }
+  if (!response.ok) {
+    throw new Error("La IA respondió con un error: " + response.statusText);
+  }
 
-    const aiResponse = await response.json();
+  const aiResponse = await response.json();
 
-    if (!aiResponse) {
-        throw new Error("No se recibió respuesta de la IA");
-    }
+  if (!aiResponse) {
+    throw new Error("No se recibió respuesta de la IA");
+  }
 
-    console.log(aiResponse);
+  console.log(aiResponse);
 
-    console.log("answer sent back");
+  console.log("answer sent back");
 
-    return aiResponse;
+  return aiResponse;
 };
 
 /**
@@ -287,8 +289,8 @@ const sendToAI = async (message, language, restriction) => {
  * @param {Object} obj - The message object to save.
  */
 export async function saveMessage(obj) {
-    const message = new Message(obj);
-    await message.save();
+  const message = new Message(obj);
+  await message.save();
 }
 
 /**
@@ -340,9 +342,9 @@ router.post('/api/quiz', verifyTokenMiddleware, async (req, res) => {
 
   } catch (error) {
     console.error('Error processing quiz:', error);
-    res.status(500).json({ 
+    res.status(500).json({
       error: "Internal server error",
-      message: error.message 
+      message: error.message
     });
   }
 });
@@ -358,7 +360,7 @@ router.post('/api/quiz', verifyTokenMiddleware, async (req, res) => {
  */
 router.post('/api/quizResponse', verifyTokenMiddleware, async (req, res) => {
   try {
-    const { quizId, answers } = req.body; 
+    const { quizId, answers } = req.body;
     const userId = req.verified_user_id;
     console.log('Received quiz response:', { quizId, answers });
 
@@ -379,11 +381,11 @@ router.post('/api/quizResponse', verifyTokenMiddleware, async (req, res) => {
         description: "Invalid answer format. Each answer must have question_id and either selected_option or value"
       });
     }
-  
+
     try {
       const quiz = await Quiz.findById(quizId);
       console.log('Found quiz:', quiz);
-  
+
       if (!quiz) {
         return res.status(404).json({
           status: "error",
@@ -419,12 +421,12 @@ router.post('/api/quizResponse', verifyTokenMiddleware, async (req, res) => {
 
       const updatedQuiz = await Quiz.findByIdAndUpdate(
         quizId,
-        { 
-          $set: { 
+        {
+          $set: {
             userAnswers: results,
             correctAnswers: correctAnswersCount,
-            'questions.$[].isAnswered': true 
-          } 
+            'questions.$[].isAnswered': true
+          }
         },
         { new: true }
       );
@@ -462,33 +464,34 @@ router.post('/api/quizResponse', verifyTokenMiddleware, async (req, res) => {
  * @throws Will throw an error if the AI service responds with an error or no response.
  */
 const sendForQuiz = async (formattedMessages) => {
-console.log("Sending message to AI:", formattedMessages);
-console.log("AIHOST actual:", AIHOST);
+  console.log("Sending message to AI:", formattedMessages);
+  console.log("AIHOST actual:", AIHOST);
 
-const response = await fetch(`${AIHOST}/generateQuiz`, {
-method: 'POST',
-headers: {
-    'Content-Type': 'application/json'
-},
-body: JSON.stringify({userPrompt: formattedMessages})
-});
+  const response = await fetch(`${AIHOST}/generateQuiz`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ userPrompt: formattedMessages }),
+    agent: new Agent({ rejectUnauthorized: false }) 
+  });
 
-console.log("answer recieved");
-if (!response.ok) {
-throw new Error("La IA respondió con un error: " + response.statusText);
-}
+  console.log("answer recieved");
+  if (!response.ok) {
+    throw new Error("La IA respondió con un error: " + response.statusText);
+  }
 
-const aiResponse = await response.json();
+  const aiResponse = await response.json();
 
-if (!aiResponse) {
-throw new Error("No se recibió respuesta de la IA");
-}
+  if (!aiResponse) {
+    throw new Error("No se recibió respuesta de la IA");
+  }
 
-console.log(aiResponse);
+  console.log(aiResponse);
 
-console.log("answer sent back");
+  console.log("answer sent back");
 
-return aiResponse;
+  return aiResponse;
 };
 
 /**
