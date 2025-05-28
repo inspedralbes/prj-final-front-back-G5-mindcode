7377da867ecfc2cd4d2c5db36b69b4db304c2c70
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-05-28 11:00:22 +0200

    prepare for exposition

--- back/config/db.js
+++ back/config/db.js
@@ -7,11 +7,13 @@ const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false,
-  }
+  },
+  process.env.DB_PORT
 );
 
 export default sequelize;
